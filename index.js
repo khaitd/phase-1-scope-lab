1534e8bbfe9950e6f942ff54053db5099f65730f
Dmitriy <khaitdmitriy@gmail.com>
2022-03-21 10:28:39 -0400

    commit tree 442a1c3674590709520a9fc14c9c294f318dd55b

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Write your solution in this file!
+
+
+var customerName = 'bob';
+
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer(){
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer(a){
+    bestCustomer = a;
+}
+
+const leastFavoriteCustomer = "5"
+
+function changeLeastFavoriteCustomer(b) {
+    leastFavoriteCustomer = b;
+}
